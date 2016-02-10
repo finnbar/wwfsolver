b71824a36d6ddb5913b8fedc337c9a981c8dff1c
Finnbar Keating <finnjkeating@aol.com>
2016-02-10 21:18:11 +0000

    Implemented blanks!

--- emailSender.py
+++ emailSender.py
@@ -1,6 +1,7 @@
-import imapclient
+import imaplib
 import smtplib
 import time
+import email # These are only listed separately so I can see if one crashes
 smtpObj = False
 imapObj = False
 myEmail = "wordswithfriendssolver@gmail.com"
@@ -13,22 +14,20 @@ def login():
     print "SMTP done!"
     # TODO: SECURELY STORE PASSWORD, FINNBAR.
     # Anyway, we now have an SMTP object, which can be passed to other functions
-    imapObj = imapclient.IMAPClient("imap.googlemail.com")
+    imapObj = imaplib.IMAP4_SSL("imap.googlemail.com")
     imapObj.login(myEmail,"thissolverisamazing")
     print "IMAP done!"
     # Now we have an IMAP object too.
     return imapObj,smtpObj
 
 def receiveMail():
-    if imapObj:
-        imapObj.select_folder("INBOX", readonly=False)
-        UIDs = imapObj.search(["UNSEEN"])
-        rawMessages = imapObj.fetch(UIDs,['BODY[]', 'FLAGS'])
-        
-        for i in rawMessages:
-            message = pyzmail.PyzMessage.factory(i['BODY[]'])
-            message.get_addresses("from")
-    
+    imapObj.select("inbox")
+    result, data = imapObj.uid("search", None, "UNSEEN")
+    # TODO: Write rest of function here, using email library to parse the email afterwards
+    # See:
+    # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
+    # https://docs.python.org/2/library/imaplib.html
+
 def sendMail(recipient,bestmove,simplemove=()):
     if smtpObj:
         mailString = "Subject: Words With Friends (requested " + time.ctime() + ")\n"
